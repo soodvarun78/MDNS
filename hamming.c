cabad7b5d0a3a18bd52306f664ebf74e54acb6cb
Varun Sood <varun.sood@oydsolution.com>
2020-03-21 16:09:15 +0530

    Modified code for hamming.c

--- hamming.c
+++ hamming.c
@@ -7,11 +7,14 @@ int main(int argc , char *argv[]) {
 	int redundantbits = 2;
 	int *datacode;
 	int xorResult=0; //XOR 
-	int i;
+	int i,c;
 	int counter=0;
 	int totalBits,parityBitCalc,size;
-	if(argc != 2){
-		printf("usage:./hamming.out <data in binary to be encoded> \n");
+	if(argc < 2){
+		printf("usage:./hamming.out <data in binary to be encoded> <m=MSB first> \n");
+		printf("e.g if 10 in decimal , in binary 1010  \n");
+		printf("./hamming.out 1010 m  \n");
+		printf("./hamming.out 0101   \n");
 		return 0;
 	}
 	datasize = strlen(argv[1]) ;
@@ -27,6 +30,20 @@ int main(int argc , char *argv[]) {
 	datacode = (int *)malloc((redundantbits + datasize )*sizeof(int));
 	memset(datacode,0,sizeof(redundantbits + datasize));
 	size = 2;
+	if(argv[2] != NULL){
+		if(strcmp(argv[2],"m")==0){
+			//printf("MSB first \n");
+			for(i=0;i<datasize/2;i++){
+				c=argv[1][datasize -1 - i];
+				argv[1][datasize-1-i]=argv[1][i];
+				argv[1][i]=c;
+			}
+		}
+	}
+	//for(i=0;i<datasize;i++)
+	//	printf("%c",argv[1][i]);
+	//printf("\n");
+	//return 0;
 //Copy the databytes to datacode .. Keeping parity bits as 0 
 	for(i=1;i<datasize+redundantbits;i++){
 		if((i + 1) % size == 0){
@@ -46,9 +63,9 @@ int main(int argc , char *argv[]) {
 //	for(i=0;i<datasize+redundantbits;i++)
 //		printf("%d",datacode[i]);
 //	printf("\n");
-
+//001000
 	counter = redundantbits-1;
-	while(counter > 0){	
+	while(counter >= 0){	
 		totalBits = datasize + redundantbits; // Total Number of bits
 		parityBitCalc = pow(2,counter); //parity bit location
 		xorResult = 0;
@@ -66,8 +83,21 @@ int main(int argc , char *argv[]) {
 	}			
 //Output
 	printf("Hamming output \n");
-	for(i=0;i<datasize+redundantbits;i++)
-		printf("%d",datacode[i]);
+	if(argv[2]){
+		if(strcmp(argv[2],"m")==0){
+			for(i=datasize+redundantbits-1;i>0;i--)
+				printf("%d",datacode[i]);
+		
+		}
+		else
+			goto LSB;
+	}
+	else
+	{
+LSB:		
+		for(i=0;i<datasize+redundantbits;i++)
+			printf("%d",datacode[i]);
+	}
 	printf("\n");
 	free(datacode);
 }
